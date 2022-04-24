c8e68ea2bfb048338a4ba5c4f5e78d99e86a01ef
Aftaab Siddiki <aftaab.siddiki@bytedance.com>
2022-04-25 00:55:17 +0800

    1224. Maximum Equal Frequency

---
+++ src/leetCode_1224/MainClass.java
@@ -0,0 +1,14 @@
+package leetCode_1224;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.maxEqualFreq(null);
+    }
+}
+
+class Solution {
+    public int maxEqualFreq(int[] nums) {
+        return 0;
+    }
+}
